e8bd50174322662116f3b4e1e0d82d3ad18fdc4d
errerew <erlina31acc@gmail.com>
2025-06-26 15:42:50 +0700

    menambahkan fungsi postorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -117,4 +117,19 @@ class BinaryTree{
             preorder(ptr->leftchild);
             preorder(ptr->rightchild);
         }
+
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+        return;
+
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " "; //parent
+        }
     };
